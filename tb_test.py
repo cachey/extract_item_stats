cec3ca465110604f46a6fc7f0b8d5449d51123cf
Kevin Shi <kshi84@student.monash.edu>
2015-06-20 23:53:19 +1000

    Graphical version now imports common methods

--- tb_test.py
+++
@@ -1,9 +0,0 @@
-import sys, traceback
-
-try:
-    f = flab
-except Exception as e:
-    print sys.exc_info()[0]
-    print sys.exc_info()[1]
-    print sys.exc_info()[2].tb_lineno
-    traceback.print_tb(sys.exc_info()[2])
